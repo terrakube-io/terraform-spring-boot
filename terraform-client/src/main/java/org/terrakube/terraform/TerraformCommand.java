4a7a70c7712f9610f91a5e1566c795db186baa08
Alfredo España <alfespa17@gmail.com>
2023-01-20 15:55:55 -0600

    Adding support to plan a terraform destroy

--- terraform-client/src/main/java/org/terrakube/terraform/TerraformCommand.java
+++ terraform-client/src/main/java/org/terrakube/terraform/TerraformCommand.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public enum TerraformCommand {
     init("init"),
     plan("plan"),
+    planDestroy("plan"),
     apply("apply"),
     destroy("destroy"),
     version("version"),
